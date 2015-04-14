6f5600ea3e786ea6c9fc2a6b2fccd7cdf057f229
Jeremiah <jeremiahj.peterson@gmail.com>
2015-04-14 13:31:40 -0700

    add no subscribed feeds warning

--- app/assets/javascripts/views/my_feed.js
+++ app/assets/javascripts/views/my_feed.js
@@ -2,6 +2,7 @@ Nconnected.Views.MyFeed = Backbone.CompositeView.extend({
   template: JST['user/feed'],
   initialize: function () {
     this.listenTo(this.collection, 'add', this.addPost.bind(this));
+    this.hasPosts = false;
   },
   render: function () {
     var content = this.template();
@@ -10,6 +11,10 @@ Nconnected.Views.MyFeed = Backbone.CompositeView.extend({
   },
   addPost: function (post) {
     var view = new Nconnected.Views.PostSwitcher({model: post});
+    if (!this.hasPosts) {
+      $('.user-feed-posts').empty();
+    }
     this.addSubview('.user-feed-posts', view);
+    this.hasPosts = true;
   }
 });
