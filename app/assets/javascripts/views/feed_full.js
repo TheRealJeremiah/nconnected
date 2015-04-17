4c23b62ba40002469debad46224d90c3a9039310
Jeremiah <jeremiahj.peterson@gmail.com>
2015-04-17 15:04:57 -0700

    add some non-functioning subscribe buttons

--- app/assets/javascripts/views/feed_full.js
+++ app/assets/javascripts/views/feed_full.js
@@ -1,11 +1,16 @@
 Nconnected.Views.FeedFull = Backbone.CompositeView.extend({
   template: JST['feeds/full'],
   className: 'feed-full-container modal fade',
+  // events: {
+  //   "click .feed-modal-subscribe": "subscribe",
+  //   "click .feed-modal-unsubscribe": "unsubscribe",
+  //   "click .close": "dismissModal"
+  // },
   initialize: function () {
     this.collection = this.model.posts();
     this.collection.fetch();
     this.listenTo(this.collection, "add", this.addPostView.bind(this));
-    this.listenTo(this.model, "sync", this.render);
+    this.listenTo(this.model, "sync change", this.render);
     this.collection.each(this.addPostView.bind(this));
   },
   render: function () {
@@ -14,8 +19,38 @@ Nconnected.Views.FeedFull = Backbone.CompositeView.extend({
     this.attachSubviews();
     return this;
   },
+  dismissModal: function (event) {
+    event.preventDefault();
+    this.$el.modal("toggle");
+  },
   addPostView: function (post) {
     var view = new Nconnected.Views.PostItem({model: post});
     this.addSubview('.feed-posts', view);
+  },
+  subscribe: function (event) {
+    event.preventDefault();
+    var subscription = new Nconnected.Models.Subscription()
+    subscription.save({feed_id: this.model.id},{
+      success: function (model) {
+        this.model.set({
+          subscribed: true,
+          subscriptionId: model.id});
+        // this.subscription = //TA: set it here? just a thought...
+      }.bind(this),
+      error: function () { // TA: check for 403 status code specifically
+        console.log("please log in");
+      }
+    })
+  },
+  unsubscribe: function (event) {
+    event.preventDefault();
+    var subscription = new Nconnected.Models.Subscription({id: this.model.get('subscriptionId')});
+    subscription.destroy({
+      success: function () {
+        this.model.set({
+          subscribed: false,
+          subscriptionId: false}); // tA: null
+      }.bind(this)
+    });
   }
 });
--- app/assets/javascripts/views/feed_full_direct.js
+++ app/assets/javascripts/views/feed_full_direct.js
@@ -1,11 +1,15 @@
 Nconnected.Views.FeedFullDirect = Backbone.CompositeView.extend({
   template: JST['feeds/fullDirect'],
   className: 'feed-full-container',
+  events: {
+    "click .feed-full-subscribe": "subscribe",
+    "click .feed-full-unsubscribe": "unsubscribe"
+  },
   initialize: function () {
     this.collection = this.model.posts();
     this.collection.fetch();
     this.listenTo(this.collection, "add", this.addPostView.bind(this));
-    this.listenTo(this.model, "sync", this.render);
+    this.listenTo(this.model, "sync change", this.render);
     this.collection.each(this.addPostView.bind(this));
   },
   render: function () {
@@ -17,5 +21,31 @@ Nconnected.Views.FeedFullDirect = Backbone.CompositeView.extend({
   addPostView: function (post) {
     var view = new Nconnected.Views.PostItem({model: post});
     this.addSubview('.feed-posts', view);
+  },
+  subscribe: function (event) {
+    event.preventDefault();
+    var subscription = new Nconnected.Models.Subscription()
+    subscription.save({feed_id: this.model.id},{
+      success: function (model) {
+        this.model.set({
+          subscribed: true,
+          subscriptionId: model.id});
+        // this.subscription = //TA: set it here? just a thought...
+      }.bind(this),
+      error: function () { // TA: check for 403 status code specifically
+        console.log("please log in");
+      }
+    })
+  },
+  unsubscribe: function (event) {
+    event.preventDefault();
+    var subscription = new Nconnected.Models.Subscription({id: this.model.get('subscriptionId')});
+    subscription.destroy({
+      success: function () {
+        this.model.set({
+          subscribed: false,
+          subscriptionId: false}); // tA: null
+      }.bind(this)
+    });
   }
 });
